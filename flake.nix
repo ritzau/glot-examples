100c5580c31a947f618d17d0f74e6b2e52da704d
Tobias Ritzau <tobias@ritzau.se>
2025-08-24 21:02:33 +0200

    Move the system loop to the project flake

--- flake.nix
+++ flake.nix
@@ -2,15 +2,27 @@
   description = "Hello World C#";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     # nix-polyglot.url = "git+file:///Users/ritzau/src/slask/aoc/nix-polyglot";
     nix-polyglot.url = "github:ritzau/nix-polyglot";
   };
 
   outputs = { self, nixpkgs, flake-utils, nix-polyglot }:
-    nix-polyglot.lib.csharp {
-      inherit self;
-      shell = "zsh";  # Optional: defaults to zsh anyway
-    };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        csharpLib = nix-polyglot.lib.csharp;
+
+        project = csharpLib {
+          inherit pkgs self;
+          # sdk = pkgs.dotnet-sdk_9;
+        };
+
+      in {
+        devShells.default = project.devShell;
+        packages.default = project.package;
+        apps.default = project.app;
+      });
 }
