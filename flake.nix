1615606f894b82691080fc4fb407807d39276fdd
Tobias Ritzau <tobias@ritzau.se>
2025-08-26 08:09:46 +0200

    Updated with simpler flake structure

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
-    nix-polyglot.url = "github:ritzau/nix-polyglot";
-    # nix-polyglot.url = "git+file:///Users/ritzau/src/slask/aoc/nix-polyglot";
+    # nix-polyglot.url = "github:ritzau/nix-polyglot";
+    nix-polyglot.url = "git+file:///Users/ritzau/src/slask/aoc/nix-polyglot";
   };
 
   outputs = { self, nixpkgs, flake-utils, nix-polyglot }:
@@ -23,22 +23,5 @@
           testProject = "HelloService.Tests/HelloService.Tests.csproj";
           nugetDeps = ./deps.json;
         };
-      in {
-        devShells.default = project.devShell;
-
-        # Default package is the standard build (typically Debug in .NET)
-        packages.default = project.package;
-        packages.dev = project.package;  # Same as default - Debug build
-
-        # For now, release build is the same until we can extend the library
-        packages.release = project.package;
-
-        # Apps
-        apps.default = project.app;
-        apps.dev = project.app;
-        apps.release = project.app;
-
-        # Use checks from the enhanced csharp.nix (includes auto-detected tests)
-        checks = project.checks;
-      });
+      in project.mkDefaultOutputs);
 }
